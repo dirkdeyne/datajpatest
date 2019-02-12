0c18c0c18aff3aabde43c73f0f9c64a6f0d1e991
dirkdeyne <dirk.deyne@gmail.com>
2019-02-12 22:56:56 +0100

    demo test

--- src/test/java/com/example/demo/MessageTest.java
+++ src/test/java/com/example/demo/MessageTest.java
@@ -4,32 +4,34 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 //import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 //import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @DataJpaTest (showSql = true )
-//@AutoConfigureTestDatabase(replace=Replace.NONE)
+/*  */
+//@AutoConfigureTestDatabase(replace=Replace.NONE) 
 public class MessageTest {
 	
 	@Autowired MessageRepo repo;
 	@Autowired JdbcTemplate template;
 	
-	@Autowired private TestEntityManager entityManager;
-	
-//	@After
-//	public void setup() {
-//		template.execute("SCRIPT /*NODATA*/ TO './data/dump.sql'");
-//	}
-	
+	@After
+	public void post() {
+		template.execute("SCRIPT NODATA TO './data/dump.sql'");
+	}
+
 	@Test
 	public void testHello() throws Exception {
-		Message m = new Message(13, "13 is my lucky number");
+		Message m = new Message(2, "Hallo World!");
 		repo.save(m);
 	}
 
